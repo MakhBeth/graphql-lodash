66702635159a34d61f6597485a42eb254444e7f9
Davide Di Pumpo <davide.dipumpo@credimi.com>
2019-09-05 11:26:23 +0200

    removes useless ternary

--- src/index.ts
+++ src/index.ts
@@ -4,49 +4,49 @@ import {
   parse,
   visit,
   print,
-  DocumentNode,
-} from 'graphql/language';
+  DocumentNode
+} from "graphql/language";
 
-import { GraphQLError } from 'graphql/error/GraphQLError';
+import { GraphQLError } from "graphql/error/GraphQLError";
 
-import { getOperationAST } from 'graphql/utilities/getOperationAST';
-import { concatAST } from 'graphql/utilities/concatAST';
-import { buildASTSchema } from 'graphql/utilities/buildASTSchema';
+import { getOperationAST } from "graphql/utilities/getOperationAST";
+import { concatAST } from "graphql/utilities/concatAST";
+import { buildASTSchema } from "graphql/utilities/buildASTSchema";
 
-import {
-  getArgumentValues,
-} from 'graphql/execution/values';
+import { getArgumentValues } from "graphql/execution/values";
 
-import get from 'lodash-es/get';
-import set from 'lodash-es/set';
-import each from 'lodash-es/each';
-import keyBy from 'lodash-es/keyBy';
-import isEqual from 'lodash-es/isEqual';
+import get from "lodash-es/get";
+import set from "lodash-es/set";
+import each from "lodash-es/each";
+import keyBy from "lodash-es/keyBy";
+import isEqual from "lodash-es/isEqual";
 
-import { applyTransformations } from './transformations';
+import { applyTransformations } from "./transformations";
 
-import { lodashIDL } from './lodash_idl';
+import { lodashIDL } from "./lodash_idl";
 
-export function graphqlLodash(query: string | DocumentNode, operationName?: string) {
+export function graphqlLodash(
+  query: string | DocumentNode,
+  operationName?: string
+) {
   const pathToArgs = {};
-  const queryAST = typeof query === 'string' ? parse(query) : query;
+  const queryAST = typeof query === "string" ? parse(query) : query;
   traverseOperationFields(queryAST, operationName, (node, resultPath) => {
     var args = getLodashDirectiveArgs(node);
-    if (args === null)
-      return;
+    if (args === null) return;
 
     // TODO: error if transformation applied on field that already
     // seen without any transformation
-    const argsSetPath = [...resultPath, '@_'];
+    const argsSetPath = [...resultPath, "@_"];
     const previousArgsValue = get(pathToArgs, argsSetPath, null);
     if (previousArgsValue !== null && !isEqual(previousArgsValue, args))
-      throw Error(`Different "@_" args for the "${resultPath.join('.')}" path`);
+      throw Error(`Different "@_" args for the "${resultPath.join(".")}" path`);
     set(pathToArgs, argsSetPath, args);
   });
 
   const stripedQuery = stripQuery(queryAST);
   return {
-    query: typeof query === 'string' ? stripQuery(print(stripedQuery)) : stripedQuery,
+    query: stripedQuery,
     transform: data => applyLodashDirective(pathToArgs, data)
   };
 }
@@ -55,23 +55,19 @@ function getLodashDirectiveArgs(node) {
   let lodashNode = null;
 
   for (let directive of node.directives || []) {
-    if (directive.name.value !== lodashDirectiveDef.name)
-      continue;
-    if (lodashNode)
-      throw Error(`Duplicating "@_" on the "${node.name.value}"`);
+    if (directive.name.value !== lodashDirectiveDef.name) continue;
+    if (lodashNode) throw Error(`Duplicating "@_" on the "${node.name.value}"`);
     lodashNode = directive;
   }
 
-  if (lodashNode === null)
-    return null;
+  if (lodashNode === null) return null;
 
   const args = getArgumentValues(lodashDirectiveDef, lodashNode);
   return normalizeLodashArgs(lodashNode.arguments, args);
 }
 
 function normalizeLodashArgs(argNodes, args) {
-  if (!argNodes)
-    return args;
+  if (!argNodes) return args;
 
   //Restore order of arguments
   argNodes = keyBy(argNodes, argNode => argNode.name.value);
@@ -79,29 +75,29 @@ function normalizeLodashArgs(argNodes, args) {
   each(argNodes, (node, name) => {
     const argValue = args[name];
 
-    if (node.value.kind === 'ObjectValue')
+    if (node.value.kind === "ObjectValue")
       orderedArgs[name] = normalizeLodashArgs(node.value.fields, argValue);
-    else if (node.value.kind === 'ListValue') {
+    else if (node.value.kind === "ListValue") {
       const nodeValues = node.value.values;
 
       orderedArgs[name] = [];
       for (let i = 0; i < nodeValues.length; ++i)
-        orderedArgs[name][i] = normalizeLodashArgs(nodeValues[i].fields, argValue[i]);
-    }
-    else if (node.value.kind === 'EnumValue' && node.value.value === 'none')
+        orderedArgs[name][i] = normalizeLodashArgs(
+          nodeValues[i].fields,
+          argValue[i]
+        );
+    } else if (node.value.kind === "EnumValue" && node.value.value === "none")
       orderedArgs[name] = undefined;
-    else
-      orderedArgs[name] = argValue;
+    else orderedArgs[name] = argValue;
   });
   return orderedArgs;
 }
 
 function applyLodashDirective(pathToArgs, data) {
-  if (data === null)
-    return null;
+  if (data === null) return null;
 
   const changedData = applyOnPath(data, pathToArgs);
-  return applyLodashArgs([], changedData, pathToArgs['@_']);
+  return applyLodashArgs([], changedData, pathToArgs["@_"]);
 }
 
 function applyLodashArgs(path, object, args) {
@@ -119,23 +115,19 @@ function applyOnPath(result, pathToArgs) {
   return traverse(result, pathToArgs);
 
   function traverse(root, pathRoot) {
-    if (root === null || root === undefined)
-      return null;
-    if (Array.isArray(root))
-      return root.map(item => traverse(item, pathRoot));
+    if (root === null || root === undefined) return null;
+    if (Array.isArray(root)) return root.map(item => traverse(item, pathRoot));
 
-    if (typeof root === 'object') {
+    if (typeof root === "object") {
       const changedObject = Object.assign({}, root);
       for (const key in pathRoot) {
-        if (key === '@_')
-          continue;
+        if (key === "@_") continue;
         currentPath.push(key);
 
         let changedValue = traverse(root[key], pathRoot[key]);
-        if (changedValue === null || changedValue === undefined)
-          continue;
+        if (changedValue === null || changedValue === undefined) continue;
 
-        const lodashArgs = pathRoot[key]['@_'];
+        const lodashArgs = pathRoot[key]["@_"];
         changedValue = applyLodashArgs(currentPath, changedValue, lodashArgs);
         changedObject[key] = changedValue;
         currentPath.pop();
@@ -149,14 +141,15 @@ function applyOnPath(result, pathToArgs) {
 
 function stripQuery(queryAST): DocumentNode {
   return visit(queryAST, {
-    [Kind.DIRECTIVE]: (node) => {
-      if (node.name.value === '_')
-        return null;
-    },
+    [Kind.DIRECTIVE]: node => {
+      if (node.name.value === "_") return null;
+    }
   });
 }
 
-export const lodashDirectiveAST: DocumentNode = parse(new Source(lodashIDL, 'lodashIDL'));
+export const lodashDirectiveAST: DocumentNode = parse(
+  new Source(lodashIDL, "lodashIDL")
+);
 const lodashDirectiveDef = getDirectivesFromAST(lodashDirectiveAST)[0];
 
 function getDirectivesFromAST(ast) {
@@ -168,8 +161,8 @@ function getDirectivesFromAST(ast) {
   const fullAST = concatAST([ast, parse(dummyIDL)]);
   const schema = buildASTSchema(fullAST);
 
-  (schema.getTypeMap()['Path'] as any).parseLiteral = (x => x.value);
-  (schema.getTypeMap()['JSON'] as any).parseLiteral = astToJSON;
+  (schema.getTypeMap()["Path"] as any).parseLiteral = x => x.value;
+  (schema.getTypeMap()["JSON"] as any).parseLiteral = astToJSON;
 
   return schema.getDirectives();
 }
@@ -201,7 +194,7 @@ function traverseOperationFields(queryAST, operationName, cb) {
   const operationAST = getOperationAST(queryAST, operationName);
   if (!operationAST) {
     throw new GraphQLError(
-      'Must provide operation name if query contains multiple operations.'
+      "Must provide operation name if query contains multiple operations."
     );
   }
 
@@ -223,14 +216,12 @@ function traverseOperationFields(queryAST, operationName, cb) {
         if (node.kind === Kind.FRAGMENT_SPREAD) {
           const fragmentName = node.name.value;
           const fragment = fragments[fragmentName];
-          if (!fragment)
-            throw Error(`Unknown fragment: ${fragmentName}`);
+          if (!fragment) throw Error(`Unknown fragment: ${fragmentName}`);
           traverse(fragment);
         }
       },
       leave(node) {
-        if (node.kind !== Kind.FIELD)
-          return;
+        if (node.kind !== Kind.FIELD) return;
         cb(node, resultPath);
         resultPath.pop();
       }
